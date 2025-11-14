92596f868fff2dee8351069f1aee855e389ba613
jackmitchellNU <jackmitchellcol@gmail.com>
2025-11-13 21:58:32 -0500

    5.2.3.4 p2

--- Lab5/index.js
+++ Lab5/index.js
@@ -1,25 +1,10 @@
-export default function QueryParameters(app) {
- const calculator = (req, res) => {
-   const { a, b, operation } = req.query;
-   let result = 0;
-   switch (operation) {
-     case "add":
-       result = parseInt(a) + parseInt(b);
-       break;
-     case "subtract":
-       result = parseInt(a) - parseInt(b);
-       break;
-     // implement multiply and divide on your own
-     case "multiply":
-       result = parseInt(a) * parseInt(b);
-       break;
-     case "divide":
-       result = parseInt(a) / parseInt(b);
-       break;
-     default:
-       result = "Invalid operation";
-   }
-   res.send(result.toString());
- };
- app.get("/lab5/calculator", calculator);
+import PathParameters from "./PathParameters.js";
+import QueryParameters from "./QueryParameters.js";
+
+export default function Lab5(app) {
+  app.get("/lab5/welcome", (req, res) => {
+    res.send("Welcome to Lab 5");
+  });
+  QueryParameters(app);
+  PathParameters(app);
 }
